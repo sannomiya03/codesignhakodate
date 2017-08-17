4e997688cc76e0805604e7beb7de7f19f14f0937
sannomiya03 <y.sannomiya03@gmail.com>
2017-08-17 22:13:07 +0900

    okmt comment2

--- src/components/common/siteFooter.vue
+++ src/components/common/siteFooter.vue
@@ -2,11 +2,17 @@
 	div.footerContainer
 		div
 			p.mail お問い合わせ先　jssd2017f<span class="spamclear">spamclear</span>@fun.ac.jp
-			p: strong 大会実行委員会
-			p 大会実行委員長：岡本誠
-			p 副実行委員長：柳英克、木村健一
-			p 実行委員：美馬義亮、原田泰、安井重哉、伊藤精英、南部美砂子、姜南圭
-		img.fun(src="static/images/icon_fun.svg")
+			p: strong 2017年度日本デザイン学会秋季機各大会実行委員会（公立はこだて未来大学）
+			p 大会実行委員長　岡本誠
+			p 副実行委員長　柳英克、木村健一
+			p 実行委員　美馬義亮、原田泰、安井重哉、伊藤精英、南部美砂子、姜南圭
+			p 学生実行委員　三野宮定里、川島奨大、斎田萌、木下誠子
+			p: strong 2017年度日本デザイン学会秋季機各大会実行委員会（公立はこだて未来大学）
+			p 実行委員長　細谷多門
+			p 実行委員　柿山浩一郎、福田大年
+		div.logos
+			div.logo: img(src="static/images/jssd_logo.png")
+			div.logo: img(src="static/images/icon_fun.svg")
 </template>
 <style lang="stylus" scoped>
 	@import "~stylus/setting"
@@ -22,9 +28,15 @@
 		margin 0
 	p strong
 		font-weight bold
-	.fun
+	.logo
+		display block
 		height 50px
-		width auto
+		
+		&:first-child
+			margin-bottom 20px
+		img
+			height 100%
+			width auto
 	.spamclear
 		display none
 	.mail
@@ -33,7 +45,9 @@
 		.footerContainer
 			flex-direction column
 			align-items center
-		.fun
+		.logos
 			margin-top 20px
+		.logo
+			text-align center
 		
 </style>
--- src/components/pages/top/program/program.vue
+++ src/components/pages/top/program/program.vue
@@ -12,7 +12,8 @@
 			div.event
 				h3.eventTitle
 					span 企画テーマセッション
-				p.description 企画テーマ「共創・当事者デザイン」に関連した、またはその他の発表を募集します。発表形式はライトニングトーク方式 です。<br>研究成果、実践結果、新しい取り組みの宣言、課題の共有など、どんな切り口でもかまいません。<br>学会員だけでなく非会員の方の参加も大歓迎です。<br>発表時間（5分）と質疑（5分）と短いですが、様々な話題を同時に共有することができます。<br>積極的なご参加をお待ちしています。
+				p.description 企画テーマ「共創・当事者デザイン」に関連した、またはその他の発表を募集します。発表形式はライトニングトーク方式（発表５分、質疑応答５分の発表形式）です。<br>研究成果、実践結果、新しい取り組みの宣言、課題の共有など、どんな切り口でもかまいません。<br>発表の登録時に、テーマや概要を大会websiteに登録してください。<br>学会員だけでなく非会員の方の参加も大歓迎です。<br>発表時間（5分）と質疑（5分）と短いですが、様々な話題を同時に共有することができます。<br>積極的なご参加をお待ちしています。<br>発表時間は短いですが、様々な話題を同時に共有することができます。<br>積極的なご参加をお待ちしています。
+				p.description <strong>司会</strong>　原田泰（公立はこだて未来大学）
 				p.applyBtn(@click="jumpToRegister(0)") 参加申し込み
 			
 			div.event
@@ -20,7 +21,7 @@
 					span 企画テーマワークショップ
 					span 「旅するデザイン函館編」
 				p.description 「旅するデザイン」とは訪問先の街を味わいその街の未来を自分事化することから始める、未来提案型のデザイン活動です。<br>ワークショップ形式で 観察、表現、省察から地域の価値と可能性を見出す活動を行います。<br>「もし自分が函館に住むことになったら」という仮定から、自身の未来の生活を妄想してみましょう。<br>一般的な観光では味わえない「函館」の魅力を探りながら、自身のデザイン観を問い直してみましょう。
-				p.description  <strong>ワークショップファシリテータ</strong>　原田泰(公立はこだて未来大学)
+				p.description  <strong>募集人数</strong>　約30名<br><strong>ワークショップファシリテータ</strong>　原田泰(公立はこだて未来大学)
 				p.applyBtn(@click="jumpToRegister(1)") 参加申し込み
 
 			div.event
@@ -29,7 +30,7 @@
 					span 「ガクガク連携」（2017年度日本デザイン学会第1支部大会）
 				p.description 皆さまの多くは、日頃から地域活性化のための様々な活動を実践されていることと思います。<br>しかしながらこうした活動は、地域の住民や各種専門分野との連携によって具体化されることもお分かりのことと思います。<br>今回の第１支部大会のテーマは「ガクガク連携」とさせていただきました。「ガクガク」は、学部と学部、学問と学問などを指しています。<br>デザイン学が他の専門分野との連携を効果的に進める上での工夫について、話題を共有し、議論の場にできればと思います。
 				p.description &nbsp;&nbsp;&nbsp;&nbsp;前半）札幌市立大学のデザインと看護の「ガクガク連携の成功と失敗」<br>&nbsp;&nbsp;&nbsp;&nbsp;後半）前半の話題と会場の意見を基に討論
-				p.description <strong>登壇予定者</strong>　安岡美佳（IT University of Copenhagen、Co-Design）、上平崇仁（専修大学）、髙田傑（建築家、函館市）、鈴木辰徳(八百屋すず辰、函館市)　他。<br><strong>司会</strong>　福田 大年(札幌市立大学 デザイン学部)<br><strong>主催</strong>　札幌市立大学(日本デザイン学会第1支部大会2017年度幹事校)
+				p.description <strong>話題提供</strong>　細谷多聞(札幌市立大学 デザイン学部)、スーディ神崎 和代(いわき明星大学 看護学部)、村松真澄(札幌市立大学 看護学部)、柿山浩一郎(札幌市立大学 デザイン学部)
 
 			div.event
 				h3.eventTitle
@@ -37,22 +38,22 @@
 				p.description デザインを学ぶ学生の皆さんのために、「学生プロポジション」（ポスターや成果物の展示によるインタラクティブ発表）を行います。本年の学生プロポジションは、10/14（土）13:00-14:40、函館市地域交流町づくりセンターにて実施します。詳細情報は、以下の参加申込のページをご覧ください。
 				p.description: a.applyBtn.targetBlank(href="http://jssd.jp/3399" target="_blank")
 					img.icon(src="static/images/icon_link.svg")
-					span 参加申し込み
+					span 詳細情報と参加申し込み
 				p.note ※応募方法、実施方法などの詳細は平成29年度学生プロポジション募集のページからご確認ください。
 
 			div.event
 				h3.eventTitle
 					span 研究部会研究会
-				p.description 研究部会の研究会の場を提供いたします。希望する研究部会は、大会ホームページを通じてお申し込みください。函館らしい素敵な施設をご用意しますので、部会で自由に御使いください。参加人数や要望を参加申し込みのフォームにご記入ください。
+				p.description 研究部会の研究会の場を提供いたします。希望する研究部会は、大会ホームページを通じてお申し込みください。函館らしい素敵な施設をご用意しますので、部会で自由にお使いください。参加人数や要望を参加申し込みのフォームにご記入ください。
 				p.description 候補：弥生町長屋、旧大黒湯、旧商船問屋の建物など
 				p.applyBtn(@click="jumpToRegister(2)") 参加申し込み
 				
 
 			div.event
 				h3.eventTitle
-					span レセプション
+					span レセプション（懇親会）
 				p.description レセプション（懇親会）の会場は、老舗西洋洋食店の「五島軒」です。このお店は、明治１２年に創業した由緒ある料理店で、伝統の味を守っています。お得な内容ですので、ぜひご参加ください。
-				p.description &nbsp;&nbsp;&nbsp;&nbsp;会員：5,000円<br>&nbsp;&nbsp;&nbsp;&nbsp;非会員：6,000円<br>&nbsp;&nbsp;&nbsp;&nbsp;学生会員：2,000円<br>&nbsp;&nbsp;&nbsp;&nbsp;学生非会員：2,000円
+				p.description &nbsp;&nbsp;&nbsp;&nbsp;会員：5,000円<br>&nbsp;&nbsp;&nbsp;&nbsp;非会員：5,000円<br>&nbsp;&nbsp;&nbsp;&nbsp;学生会員：2,000円<br>&nbsp;&nbsp;&nbsp;&nbsp;学生非会員：2,000円
 
 		h2.subSectionTitle 大会日程
 		table
@@ -85,8 +86,6 @@
 							li 集合時間 9時
 							li 会場　デザインベース02 旧大黒湯
 							li 活動時間　09:00 - 12:00
-						p.applyBtn(@click="jumpToRegister(1)") 参加申し込み
-						p.note ※2日続けての参加が条件となります
 			tr
 				th.time
 				td
@@ -169,7 +168,7 @@
 							li 会場　函館市地域交流まちづくりセンター2F
 						a.applyBtn.targetBlank(href="http://jssd.jp/3399" target="_blank")
 							img.icon(src="static/images/icon_link.svg")
-							span 参加申し込み
+							span 詳細情報と参加申し込み
 						p.note ※応募方法、実施方法などの詳細は平成29年度学生プロポジション募集のページからご確認ください。
 				td.event.green
 					div.inner(:style="height(8)")
@@ -324,7 +323,7 @@
 				th.time 19:00
 				td.event.purple
 					div.inner(:style="height(4)")
-						h3.eventTitle レセプション
+						h3.eventTitle レセプション（懇親会）
 						ul.detail
 							li 時間　19:00 - 21:00
 							li 会場　五島軒（函館市末広町）
